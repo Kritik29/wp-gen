762a11193d10d632174efba5d1ef976134e60d2f
Kritik29 <Kritik29@users.noreply.github.com>
2021-06-19 22:14:04 -0400

    Updates

---
+++ precache-manifest.6f48666371ac55ea08861b27bed02f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694ed09332c914e6899c909eb2f735ff",
+    "url": "/Kritik29/wp-gen/index.html"
+  },
+  {
+    "revision": "606a86c5791b3e2b7e42",
+    "url": "/Kritik29/wp-gen/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff098708efbe034aff03",
+    "url": "/Kritik29/wp-gen/static/js/2.8586f689.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kritik29/wp-gen/static/js/2.8586f689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606a86c5791b3e2b7e42",
+    "url": "/Kritik29/wp-gen/static/js/main.99b55a1b.chunk.js"
+  },
+  {
+    "revision": "8d2fbae85b315a3f10df",
+    "url": "/Kritik29/wp-gen/static/js/runtime-main.2780e757.js"
+  }
+]);
